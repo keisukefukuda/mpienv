52f64b5e533d06b36c7567c1faef74ed5d91a881
Keisuke Fukuda <kfukuda@preferred.jp>
2017-05-23 10:18:39 +0900

    sort in mpienv-list

--- mpienv_list.py
+++ mpienv_list.py
@@ -9,7 +9,9 @@ if __name__ == '__main__':
     max_label_len = max(len(name) for name in manager.keys())
 
     print("\nInstalled MPIs:\n")
-    for name, info in manager.items():
+    lst = [info for name,info in manager.items()]
+    lst.sort(key=lambda x: x['name'])
+    for info in lst:
         print(" {} {:<{width}} -> {}".format(
             "*" if info['active'] else " ",
             info['name'],
