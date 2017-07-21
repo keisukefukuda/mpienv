cee48bdd53b1eb2c6ecc1a1c4e037c920b225689
Keisuke Fukuda <kfukuda@preferred.jp>
2017-07-21 17:20:42 +0900

    test fix for Python 2

--- tests/test_main.sh
+++ tests/test_main.sh
@@ -239,13 +239,14 @@ test_reg_issue10(){
 
     # If the `use` command does not run `pip install mpi4py`,
     # which is a correct behavior, E-S should be < 1 [s].
-    local OK=$(python -c "print('0') if ${E}.0 - ${S} < 1.0 else print('1')")
+    local OK=$(python -c "print('0' if ${E}.0 - ${S} < 1.0 else '1')")
     assertTrue "$OK"
 }
 
-# suite() {
-#     suite_addTest "test_mpi4py_clear_pypath"
-# }
+suite() {
+    suite_addTest "test_reg_issue10"
+    #suite_addTest "test_mpi4py_clear_pypath"
+}
 
 
 #-----------------------------------------------------------
