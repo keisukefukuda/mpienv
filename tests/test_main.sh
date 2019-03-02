6aa6279428fcb9d6d27f0930e7ee2eaff6812bbb
Keisuke Fukuda <kfukuda@preferred.jp>
2019-03-02 18:37:55 +0900

    fix travis error

--- tests/test_main.sh
+++ tests/test_main.sh
@@ -56,7 +56,7 @@ echo "proj_dir=$proj_dir"
 echo "=================== Install mpienv =================="
 cd ${proj_dir}
 rm -rf mpienv.egg-info dist ||:
-pip install --user -e .
+pip install -e .
 
 echo "=================== Load mpienv =================="
 which mpienv-init
