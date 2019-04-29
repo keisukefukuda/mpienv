ff80250fee78b0f3fbf486d6a5cd97db29e2e330
Keisuke Fukuda <kfukuda@preferred.jp>
2019-04-29 22:40:50 +0900

    Improve autodiscover message

--- mpienv/command/autodiscover.py
+++ mpienv/command/autodiscover.py
@@ -141,7 +141,7 @@ def install_mpi(path, mpiexec):
         else:
             raise
     except AlreadyManagedMpi as e:
-        prints("{} is already managed".format(path))
+        pass
     except RuntimeError as e:
         prints("Error occured while "
                "adding {}".format(path))
@@ -169,9 +169,11 @@ def investigate_path(path, flg_to_add, done={}):
                    "'{}'".format(path, name))
             prints()
         else:
+            prints("")
             prints("--------------------------------------")
             prints("Found {}".format(mpiexec))
-            prints(pprint.pformat(mpienv.get_mpi_from_mpiexec(mpiexec)))
+            mpi =mpienv.get_mpi_from_mpiexec(mpiexec)
+            mpi.describe()
             # Install the new MPI
             if flg_to_add:
                 install_mpi(path, mpiexec)
